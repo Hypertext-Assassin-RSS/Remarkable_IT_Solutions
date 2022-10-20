92c7439fb29da98c3603c327d746636ffe484c17
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-20 20:34:32 +0530

    add under construction message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import '@fontsource/roboto/700.css';
+import { Typography } from '@mui/material';
 import './App.css';
+import message from './resource/img/message.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Home-Container'>
+      <Typography variant="h1" fontWeight='bolder' color='#6A2C70' gutterBottom>
+        Remarkable IT Solutions
+      </Typography>
+      <Typography variant="h2" fontWeight='bold' gutterBottom>
+        Welcome!
+      </Typography>
+      <div className='Image-Container'>
+        <img src={message} />
+      </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
